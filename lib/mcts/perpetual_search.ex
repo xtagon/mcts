67f471c6560cc84ab614c30900353a9ee86f6e60
Justin Workman <xtagon@gmail.com>
2020-11-08 23:07:06 -0800

    Remove DynamicSupervisor logic from PerpetualSearch.

--- lib/mcts/perpetual_search.ex
+++ lib/mcts/perpetual_search.ex
@@ -3,22 +3,7 @@ defmodule MCTS.PerpetualSearch do
 
   alias MCTS.Search
 
-  @default_registry MCTS.PerpetualSearch.SearchRegistry
-  @default_supervisor MCTS.PerpetualSearch.SearchSupervisor
-
   @default_game MCTS.Examples.LeftRightGame
-
-  def start(search_id, opts \\ []) do
-    {registry, opts} = Keyword.pop(opts, :registry, @default_registry)
-    {supervisor, opts} = Keyword.pop(opts, :supervisor, @default_supervisor)
-
-    opts = Keyword.merge(opts, [
-      search_id: search_id,
-      name: {:via, Registry, {registry, search_id}}
-    ])
-
-    DynamicSupervisor.start_child(supervisor, {__MODULE__, opts})
-  end
 
   def start_link(opts \\ []) do
     {game, opts} = Keyword.pop(opts, :game, @default_game)
@@ -29,16 +14,7 @@ defmodule MCTS.PerpetualSearch do
       next_fun: {Search, :search, []}
     ]
 
-    opts = Keyword.put_new(opts, :name, __MODULE__)
-
     Perpetual.start_link(args, opts)
-  end
-
-  def lookup(search_id, registry \\ @default_registry) do
-    case Registry.lookup(registry, search_id) do
-      [{pid, _}] -> {:ok, pid}
-      [] -> {:error, :not_found}
-    end
   end
 
   def update(perpetual, new_game_state, timeout \\ 5000) do
